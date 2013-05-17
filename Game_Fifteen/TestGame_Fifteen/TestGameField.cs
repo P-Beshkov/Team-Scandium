b249ec4adb744f7509f8f7dd71d36b257ad3ff71
nikolay.marinov <nikolay.marinov76@gmail.com>
2013-05-17 12:03:27 +0300

    class TestGameField - created

---
+++ Game_Fifteen/TestGame_Fifteen/TestGameField.cs
@@ -0,0 +1,22 @@
+﻿//-----------------------------------------------------------------------
+// <copyright file="TestGameField.cs" company="TelerikAcademy">
+//     All rights reserved © Telerik Academy 2012-2013
+// </copyright>
+//----------------------------------------------------------------------
+namespace TestGame_Fifteen
+{
+    using System;
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+    using Game_Fifteen;
+
+    [TestClass]
+    public class TestGameField
+    {
+        [TestMethod]
+        public void TestGameField1()
+        {
+            GameField field = new GameField();
+            field.
+        }
+    }
+}
